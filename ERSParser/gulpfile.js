3507afead3f9a1d877b16df315f5f11d6b96ab3a
Martin Rouček <roucekmar@gmail.com>
2016-12-02 16:21:54 +0100

    Added gulp precommit check

--- ERSParser/gulpfile.js
+++ ERSParser/gulpfile.js
@@ -1,18 +1,22 @@
 var gulp = require('gulp');
+var _ = require('underscore');
 var exec = require('child_process').exec;
 var spawn = require('child_process').spawn;
 var mocha = require('gulp-mocha');
 var clean = require('gulp-clean');
 var jison = require('gulp-jison');
 var rename = require('gulp-rename');
-browserify = require('gulp-browserify')
+var browserify = require('gulp-browserify');
+var simpleGit = require('simple-git');
 
-gulp.task('clean', function() {
-    return gulp.src(['generatedParser','bin'], { read: false })
+var allowedPathsToCommitOutOfMaster = ['src/parser.jison', 'test/queries'];
+
+gulp.task('clean', function () {
+    return gulp.src(['generatedParser', 'bin'], { read: false })
         .pipe(clean());
 });
 
-gulp.task('buildParser', ['clean'], function() {
+gulp.task('buildParser', ['clean'], function () {
     var pipeResult = gulp.src('.\\src\\parser.jison')
         .pipe(jison({ type: "slr" }))
         .pipe(rename('parserModule.js'))
@@ -20,20 +24,48 @@ gulp.task('buildParser', ['clean'], function() {
     return pipeResult;
 });
 
-gulp.task('build', ['buildParser','browserify'], function() {
+gulp.task('build', ['buildParser', 'browserify'], function () {
 });
 
-gulp.task('test', ['build'], function() {
+gulp.task('test', ['build'], function () {
     return gulp.src('test/*.js', { read: false })
         .pipe(mocha({ reporter: 'progress' }));
 })
 
-gulp.task('browserify',['clean','buildParser'], function() {
+gulp.task('browserify', ['clean', 'buildParser'], function () {
     gulp.src(['src/ERSParser.js'])
-        .pipe(browserify({standalone:'ERSParser'}))
+        .pipe(browserify({ standalone: 'ERSParser' }))
         .pipe(gulp.dest('./bin'))
 });
 
-gulp.task('default', function() {
+gulp.task('default', function () {
     console.log('There is no default action');
+});
+
+gulp.task('precommit', function () {
+    var git = simpleGit('..');
+    var branch;
+    var diffSummary;
+    var neco = git.branchLocal(function (err, summary) {
+        branch = summary.current;
+        console.log();
+    })
+        .diffSummary(['--cached'], function (err, summary) {
+            diffSummary = summary;
+        })
+        .then(function () {
+            if (branch == "master") {
+                return;
+            }
+            var files = diffSummary.files;
+            for (var i = 0; i < files.length; i++) {
+                var currentFilePath = files[i].file;
+                var result = _(allowedPathsToCommitOutOfMaster).any(function (allowedPath) {
+                    return currentFilePath.match(allowedPath);
+                });
+                if (!result) {
+                    console.log("Warning: you probably did not want commit",'\033[31m', currentFilePath, '\x1b[0m');
+                }
+            }
+        });
 });
