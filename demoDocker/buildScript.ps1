7b2dc7aa091da4c4f3fdebdeb07c6793f2c00f54
Martin Rouček <roucekmar@gmail.com>
2018-06-13 18:41:58 +0200

    Docker demo added

---
+++ demoDocker/buildScript.ps1
@@ -0,0 +1,4 @@
+Remove-Item bin -Force  -Recurse -ErrorAction SilentlyContinue
+mkdir bin | Out-Null
+# Unfortunately windows docker dont work with local paths, must be changed 
+docker run --rm -it -v C:/Users/martin/sparqleditor/demoDocker/bin:/tmp/bin sparqleditor /bin/bash -c "cp -r ./dist/* /tmp/bin;cp -r ./node_modules/monaco-editor /tmp/bin/monaco-editor;cp -r ./node_modules/requirejs /tmp/bin/requirejs"
